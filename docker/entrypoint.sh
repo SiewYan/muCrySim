ca6e1cdd61a2349ad7096eca8a99480f896b5633
SiewYan <siew.yan.hoh@gmail.com>
2024-11-26 14:33:30 +0800

    setup user area

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,9 @@
 
 echo setting up dependencies
 source /opt/Miux3-installer/local/bin/geant4.sh
-mkdir -p run
-cp /mac/visVRML.mac run
-mkdir -p run/data
+mkdir -p docker/run
+cp /mac/visVRML.mac docker/run
+mkdir -p docker/run/data
 cd run
 echo "musrSim_upgrade $@"
 /opt/muCrySim/local/bin/musrSim_upgrade $@
