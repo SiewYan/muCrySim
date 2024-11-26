08a9aa9f56631307aca353c7890b4c8a97a19355
SiewYan <siew.yan.hoh@gmail.com>
2024-11-26 15:42:02 +0800

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,9 @@
 
 echo setting up dependencies
 source /opt/Miux3-installer/local/bin/geant4.sh
-mkdir -p /opt/Miux3-installer/docker/run
-cp /opt/Miux3-installer/mac/visVRML.mac /opt/Miux3-installer/docker/run
-mkdir -p /opt/Miux3-installer/docker/run/data
-cd /opt/Miux3-installer/docker/run
+mkdir -p /opt/muCrySim/docker/run
+cp /opt/muCrySim/mac/visVRML.mac /opt/muCrySim/docker/run
+mkdir -p /opt/muCrySim/docker/run/data
+cd /opt/muCrySim/docker/run
 echo "musrSim_upgrade $@"
 /opt/muCrySim/local/bin/musrSim_upgrade $@
